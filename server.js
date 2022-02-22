d95bebd9fe5d5879ce9c2be4b763ff74a0ebb9d3
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-23 01:37:22 +0600

    build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
-  static: './dist'
+  static: './build'
 });
 const PORT = 8000;
 server.use(middlewares);
