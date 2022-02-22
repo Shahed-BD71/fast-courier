ac7dbdb1c6393dff3e9dbc4376a2582b43e89ca5
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-23 01:49:56 +0600

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
-  static: './build'
+  static: './dist'
 });
 const PORT = 8000;
 server.use(middlewares);
