18f29ae263df4c3f6bfdcca62eba74850a5df9f0
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-23 02:07:36 +0600

    fixed

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+require("dotenv").config();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './dist'
 });
-const PORT = 8000;
+const port = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
 }))
 server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running at' + " " + PORT);
+server.listen(port, () => {
+  console.log(`Server is running at ${port}`);
 });
